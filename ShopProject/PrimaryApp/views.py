9fbbca0fffd1127373f2c53d0e6c81a9a457bb51
Dawid Janowski <dawidjanowski@interia.pl>
2023-06-29 15:55:19 +0200

    Added PrimaryApp to settings and created HTML file in templates, also changed the HTTPRequest in views and added imports in that file

--- ShopProject/PrimaryApp/views.py
+++ ShopProject/PrimaryApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def main():
+    print("Hello world! My name is...")
 
 def home(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('MainPage/myfirst.html')
+    return HttpResponse(template.render())
